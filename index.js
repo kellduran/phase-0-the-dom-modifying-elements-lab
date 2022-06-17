78c9afa906e34103828d83e2a4e02cbc1013eb6a
Kelly Duran <kaduran16@gmail.com>
2022-06-17 13:18:24 -0400

    commit tree 025a418e59529797ff56034580d767d798919018

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+main.remove();// Write your code here!
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+console.log(newHeader.id);
+newHeader.textContent ='YOUR-NAME is the champion';
+
+document.body.append(element);
+
+
+
+
+
+
+
+
+
